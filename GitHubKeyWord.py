8f429622997b7a777fe78e8de5c7a04030ace4c4
MansoorMir1 <mansoormir6432@gmail.com>
2025-11-01 01:16:08 +0500

    Udate file

--- GitHubKeyWord.py
+++
@@ -1,8 +0,0 @@
-# 
-# git init
-#        git remote add origin <-link->
-#        git remote -v            (To verify remote)
-#        git branch               (To check branch)
-#        git branch -M main       (To rename branch)
-#        git push origin main
-# 
