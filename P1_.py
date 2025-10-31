171f9ad1d6ca9305934628680f2bd4c699bf9367
MansoorMir1 <mansoormir6432@gmail.com>
2025-10-31 17:05:10 +0500

    Add new file

---
+++ P1_.py
@@ -0,0 +1,8 @@
+# 
+# git init
+#        git remote add origin <-link->
+#        git remote -v            (To verify remote)
+#        git branch               (To check branch)
+#        git branch -M main       (To rename branch)
+#        git push origin main
+# 
